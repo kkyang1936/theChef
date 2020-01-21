fa8ee86b59b8e2a2424160972bbb8962aef26e14
Nicholas Kashani Motlagh <kashanimotlagh.1@osu.edu>
2020-01-21 18:21:19 -0500

    added

---
+++ Chef/Chef/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Chef
+//
+//  Created by Nicholas Motlagh on 1/21/20.
+//  Copyright © 2020 Executive Chef. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
