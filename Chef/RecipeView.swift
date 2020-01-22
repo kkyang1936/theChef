d8432415d270ee7c54e5c4530a734c60eb1cd9bd
KKYang <yang.4098@osu.edu>
2020-01-22 16:10:08 -0500

    Add recipe view file

---
+++ Chef/RecipeView.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeView.swift
+//  Chef
+//
+//  Created by KK Yang on 2020/1/22.
+//  Copyright © 2020 Executive Chef. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+struct RecipeView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
