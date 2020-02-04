02bed9701d413da21358d898fd2db0f5e6ccec11
ZetsubouShita <clemens.114>
2020-02-04 17:52:43 -0500

    Cleaned up the code for recipe view

--- Chef/RecipeView.swift
+++ Chef/RecipeView.swift
@@ -11,10 +11,13 @@ import SwiftUI
 
 
 struct RecipeView: View {
+    @State private var recipeName = "Baked Denver Omelet"
+    @State private var ingredients = [("butter", "2 tablespoons"), ("onion", "1/2, chopped"), ("green bell pepper", "1/2, chopped"), ("cooked ham", "1 cup, chopped"), ("eggs", "8"), ("milk", "1/4 cup"), ("cheddar cheese", "1/2 cup, shredded"), ("salt and ground black pepper", "to taste")]
+    @State private var steps = ["Preheat oven to 400 degrees F (200 degrees C). Grease a 10-inch round baking dish.", "Melt butter in a large skillet over medium heat; cook and stir onion and bell pepper until softened, about 5 minutes. Stir in ham and continue cooking until heated through, 5 minutes more.", "Beat eggs and milk in a large bowl. Stir in Cheddar cheese and ham mixture; season with salt and black pepper. Pour mixture into prepared baking dish.", "Bake in preheated oven until eggs are browned and puffy, about 25 minutes. Serve warm."]
     var body: some View {
         VStack {
             HStack {
-                Text("Baked Denver Omelet")
+                Text(recipeName)
                     .font(.title)
                     .fontWeight(.medium)
                     .offset(x: 20)
@@ -31,57 +34,21 @@ struct RecipeView: View {
                     Spacer()
                     
                 }
+                ForEach(0..<ingredients.count) { index in
                     HStack {
-                        Text("butter")
+                        Text(self.ingredients[index].0)
                         Spacer()
-                        Text("2 tablespoons")
+                        Text(self.ingredients[index].1)
                     }
-                    HStack {
-                        Text("onion")
-                        Spacer()
-                        Text("1/2, chopped")
-                    }
-                    HStack {
-                        Text("green bell pepper")
-                        Spacer()
-                        Text("1/2, chopped")
-                    }
-                    HStack {
-                        Text("cooked ham")
-                        Spacer()
-                        Text("1 cup, chopped")
-                    }
-                    HStack {
-                        Text("eggs")
-                        Spacer()
-                        Text("8")
-                    }
-                    HStack {
-                        Text("milk")
-                        Spacer()
-                        Text("1/4 cup")
-                    }
-                    HStack {
-                        Text("cheddar cheese")
-                        Spacer()
-                        Text("1/2 cup, shredded")
-                    }
-                    HStack {
-                        Text("salt and ground black pepper")
-                        Spacer()
-                        Text("to taste")
-                    }
-                Group {
-                    HStack {
-                        Spacer()
-                        Text("Steps")
-                            .font(.headline)
-                        Spacer()
-                    }
-                    Text("Preheat oven to 400 degrees F (200 degrees C). Grease a 10-inch round baking dish.")
-                    Text("Melt butter in a large skillet over medium heat; cook and stir onion and bell pepper until softened, about 5 minutes. Stir in ham and continue cooking until heated through, 5 minutes more.")
-                    Text("Beat eggs and milk in a large bowl. Stir in Cheddar cheese and ham mixture; season with salt and black pepper. Pour mixture into prepared baking dish.")
-                    Text("Bake in preheated oven until eggs are browned and puffy, about 25 minutes. Serve warm.")
+                }
+                HStack {
+                    Spacer()
+                    Text("Steps")
+                        .font(.headline)
+                    Spacer()
+                }
+                ForEach(steps, id:\.self) { step in
+                    Text(step)
                 }
             }
             Button(action: {}) {
