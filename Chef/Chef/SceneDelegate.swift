bfa4c74406f6e4884e5f16ce063ce837c1026beb
ZetsubouShita <clemens.114>
2020-02-12 20:39:57 -0500

    Added a hopefully working network image view

--- Chef/Chef/SceneDelegate.swift
+++ Chef/Chef/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = NetworkImage(url: URL(string: "https://images.media-allrecipes.com/userphotos/560x315/1029125.jpg")!)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
---
+++ Chef/Chef/Views/NetworkImage.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkImage.swift
+//  Chef
+//
+//  Created by Jason Clemens on 2/12/20.
+//  Copyright © 2020 Executive Chef. All rights reserved.
+//
+
+import SwiftUI
+import URLImage
+
+struct NetworkImage: View {
+
+    let url: URL
+
+    var body: some View {
+        URLImage(url, placeholder: {
+            ProgressView($0) { progress in
+                Image("recipe-default-image")
+            }
+        })
+    }
+}
+
+struct NetworkImage_Previews: PreviewProvider {
+    static var previews: some View {
+        NetworkImage(url: URL(string: "https://images.media-allrecipes.com/userphotos/560x315/1029125.jpg")!)
+    }
+}
