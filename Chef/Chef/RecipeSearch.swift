289f28b428e4f63674c110abf3d5de54f04b7171
Aaron Nikolai <aaronnikolai2015@gmail.com>
2020-02-17 12:26:58 -0500

    Added class that can search AllRecipes based on ingredients given

---
+++ Chef/Chef/RecipeSearch.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeSearch.swift
+//  Chef
+//
+//  Created by Aaron Nikolai on 2/17/20.
+//  Copyright © 2020 Executive Chef. All rights reserved.
+//
+
+import Foundation
+
+class RecipeSearch {
+    private let baseURL = "https://www.allrecipes.com/search/results/?ingIncl="
+    private var finalURL = URL(string: "")
+    
+    private func createURL(ingredients: [String]) -> URL {
+        var stringURL = String()
+        stringURL.append(baseURL)
+        stringURL.append(ingredients.joined(separator: ","))
+        stringURL.append("&sort=re")
+        return URL(string: stringURL)!
+    }
+    
+    func getURLWithIngredients(ingredients: [String]) -> URL {
+        return createURL(ingredients: ingredients)
+    }
+    
+    private func sampleIngredients() -> [String] {
+        return ["eggs","bacon","cheese"]
+    }
+}
---
+++ Chef/Chef/RecipeSearchScraper.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeSearchScraper.swift
+//  Chef
+//
+//  Created by Aaron Nikolai on 2/17/20.
+//  Copyright © 2020 Executive Chef. All rights reserved.
+//
+
+import Foundation
+import SwiftSoup
+
+class RecipeSearchScraper {
+    
+}
